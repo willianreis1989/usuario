20e8e213c4de49b340f8e6607e2c3d2159f91b27
Willian Reis <williangaditareis@gmail.com>
2025-10-08 19:10:25 -0300

    adicionado método que salva dados usuário

--- src/main/java/com/javanauta/usuario/insfrastructure/entity/Endereco.java
+++ src/main/java/com/javanauta/usuario/insfrastructure/entity/Endereco.java
@@ -1,10 +1,7 @@
 package com.javanauta.usuario.insfrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -12,6 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "enderecos")
+@Builder
 
 public class Endereco {
 
